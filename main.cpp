72abaf0c3e0404e59865675e61ec039f64b46057
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-04-12 19:56:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 
 int main() {
-   return 0;
+    float a = 0, b = 0,x = 0,y = 0,z = 0;
+    cout << "inserisci i 4 valori \n";
+    cin >> a >> b >> x >> y;
+
+    if(x < 0 && y > 0){
+        z = a*x - b*y;
+    }
+    else if(x >= 0 && y <= 0){
+        z = a*pow(x,2) - b*y;
+    }
+    else {
+        z = a*x + b*pow(y,2);
+    }
+    cout << "Il risultato e': " << z << endl;
+    return 0;
 }
